0e5a98d09f4434a04417fa36338b73f110d8f70e
nethoncho <nethoncho@gmail.com>
2015-10-22 19:49:27 -0400

    set edit in place options

--- assets/js/exampleApp.js
+++ assets/js/exampleApp.js
@@ -3,4 +3,9 @@
 var vApp = angular.module('exampleApp', ['ngTouch', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'angular-loading-bar', 'toastr', 'xeditable', , 'ngSailsBind'])
   .config(['$urlRouterProvider', function ($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
+  }])
+  .run(['editableOptions', 'editableThemes', function(editableOptions, editableThemes) {
+    editableThemes.bs3.inputClass = 'input-sm';
+    editableThemes.bs3.buttonsClass = 'btn-sm';
+    editableOptions.theme = 'bs3';
   }]);
