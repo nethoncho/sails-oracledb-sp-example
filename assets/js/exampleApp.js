55945dbfbd0a31d2cdf39f36c871c4fff2b3dbf0
nethoncho <nethoncho@gmail.com>
2015-10-22 18:51:27 -0400

    added ngSailsBind

--- assets/js/exampleApp.js
+++ assets/js/exampleApp.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var vApp = angular.module('exampleApp', ['ngTouch', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'angular-loading-bar', 'toastr', 'xeditable'])
+var vApp = angular.module('exampleApp', ['ngTouch', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'angular-loading-bar', 'toastr', 'xeditable', , 'ngSailsBind'])
   .config(['$urlRouterProvider', function ($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
   }]);
