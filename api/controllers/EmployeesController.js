c38cc0b8152eee0a80e01d8859780952f22505a5
nethoncho <nethoncho@gmail.com>
2015-10-22 17:59:55 -0400

    added find

--- api/controllers/EmployeesController.js
+++ api/controllers/EmployeesController.js
@@ -6,13 +6,30 @@
  */
 
 module.exports = {
-  create: function(req, res) {
-    sails.log.verbose('Project: create');
-    sails.log.silly(req.body);
-
+  find: function(req, res) {
     var allParams = req.allParams();
-    sails.log.silly(allParams);
 
+    var params = [];
+    _.forOwn(allParams, function(value, key) {
+      if(!_.isUndefined(value)) {
+        var param = {};
+        param[key] = value;
+        params.push(param);
+      }
+    });
+
+    if(params.length === 0) {
+      params = null;
+    }
+
+    Employees.find(params, function(err, employeess) {
+      if (err) return res.negotiate(err);
+      res.json(employeess);
+    });
+  },
+
+  create: function(req, res) {
+    var allParams = req.allParams();
     Employees.create(allParams, function(err, employee) {
       if (err) return res.negotiate(err);
       res.json(employee);
