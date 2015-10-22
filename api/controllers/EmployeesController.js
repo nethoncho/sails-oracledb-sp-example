781f0b8aefff849cb691804b6ff80be396aa49e2
nethoncho <nethoncho@gmail.com>
2015-10-22 17:42:12 -0400

    create build out

--- api/controllers/EmployeesController.js
+++ api/controllers/EmployeesController.js
@@ -6,6 +6,18 @@
  */
 
 module.exports = {
+  create: function(req, res) {
+    sails.log.verbose('Project: create');
+    sails.log.silly(req.body);
+
+    var allParams = req.allParams();
+    sails.log.silly(allParams);
+
+    Employees.create(allParams, function(err, employee) {
+      if (err) return res.negotiate(err);
+      res.json(employee);
+    });
+  }
 	
 };
 
