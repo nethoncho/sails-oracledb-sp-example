574921607e635536d9a7d11fdb3b0aeae94df749
nethoncho <nethoncho@gmail.com>
2015-10-23 09:57:54 -0400

    cleanup indent

--- assets/js/employeesCtrl.js
+++ assets/js/employeesCtrl.js
@@ -7,23 +7,23 @@ angular.module('exampleApp').controller('employeesCtrl', ['$scope', '$sailsBind'
     $scope.updateSalary = function(employee, data) {
       var d = $q.defer();
 
-       $http.put('api/employee/' + employee.id, {
-         salary: data
-       })
-       .success(function(response) {
-         d.resolve(false);
-       })
-       .error(function(response) {
-         if(angular.isObject(response)) {
-           if(response.hasOwnProperty('message')) {
-             d.reject(response.message);
-           } else {
-             d.reject('API Error!');
-           }
-         } else {
-           d.reject('Unknown API Error!');
-         }
-       });
+      $http.put('api/employee/' + employee.id, {
+        salary: data
+      })
+      .success(function(response) {
+        d.resolve(false);
+      })
+      .error(function(response) {
+        if(angular.isObject(response)) {
+          if(response.hasOwnProperty('message')) {
+            d.reject(response.message);
+          } else {
+            d.reject('API Error!');
+          }
+        } else {
+          d.reject('Unknown API Error!');
+        }
+      });
 
       return d.promise;
     };
