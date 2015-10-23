14d9ae10604f17d7002dc14b59e6b3e54e24110e
nethoncho <nethoncho@gmail.com>
2015-10-23 10:01:23 -0400

    build out

--- assets/js/departmentsCtrl.js
+++ assets/js/departmentsCtrl.js
@@ -1,6 +1,36 @@
 'use strict';
 
-angular.module('exampleApp').controller('departmentsCtrl', ['$scope',
-  function ($scope) {
+angular.module('exampleApp').controller('departmentsCtrl', ['$scope', '$sailsBind', '$q', '$http',
+  function ($scope, $sailsBind, $q, $http) {
+    $sailsBind.bind('api/department', $scope);
+
+    $scope.updateName = function(department, data) {
+      var d = $q.defer();
+
+      if(data) {
+        data = data.trim();
+      }
+
+      $http.put('api/department/' + department.id, {
+        salary: data
+      })
+      .success(function(response) {
+        d.resolve(false);
+      })
+      .error(function(response) {
+        if(angular.isObject(response)) {
+          if(response.hasOwnProperty('message')) {
+            d.reject(response.message);
+          } else {
+            d.reject('API Error!');
+          }
+        } else {
+          d.reject('Unknown API Error!');
+        }
+      });
+
+      return d.promise;
+    };
+
   }
 ]);
