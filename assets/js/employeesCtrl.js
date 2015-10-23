d4dbe7d10353140b09296a938c064189f3d2725f
nethoncho <nethoncho@gmail.com>
2015-10-23 11:24:18 -0400

    edit in place for department

--- assets/js/employeesCtrl.js
+++ assets/js/employeesCtrl.js
@@ -1,8 +1,37 @@
 'use strict';
 
-angular.module('exampleApp').controller('employeesCtrl', ['$scope', '$sailsBind', '$q', '$http', 'toastr',
-  function ($scope, $sailsBind, $q, $http, toastr) {
+angular.module('exampleApp').controller('employeesCtrl', ['$scope', '$sailsBind', '$q', '$http', 'departments',
+  function ($scope, $sailsBind, $q, $http, departments) {
+    $scope.departments = departments;
     $sailsBind.bind('api/employee', $scope);
+
+    $scope.showDepartment = function(employee) {
+      return _.result(_.find($scope.departments, { id: employee.department }), 'name');
+    };
+
+    $scope.updateDepartment = function(employee, data) {
+      var d = $q.defer();
+
+      $http.put('api/employee/' + employee.id, {
+        department: data
+      })
+      .success(function(response) {
+        d.resolve(false);
+      })
+      .error(function(response) {
+        if(angular.isObject(response)) {
+          if(response.hasOwnProperty('message')) {
+            d.reject(response.message);
+          } else {
+            d.reject('API Error!');
+          }
+        } else {
+          d.reject('Unknown API Error!');
+        }
+      });
+
+      return d.promise;
+    };
 
     $scope.updateSalary = function(employee, data) {
       var d = $q.defer();
